c6ef3f10ae77d279960c706680596658573f6554
Кирилл <145338913+kshr0x@users.noreply.github.com>
2025-03-18 10:25:41 +0300

    1

--- pr3_b1/module_a.py
+++
@@ -1,6 +0,0 @@
-import module_b
-
-def func_a():
-    print("Function A called")
-
-module_b.func_b()
--- pr3_b1/module_b.py
+++
@@ -1,6 +0,0 @@
-import module_a
-
-def func_b():
-    print("Function B called")
-
-module_a.func_a()
